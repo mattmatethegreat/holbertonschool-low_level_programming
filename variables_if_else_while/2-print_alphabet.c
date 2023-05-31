b879f7e95cdad345d35fed0b324a2e12117066a7
root <matthgraham1996@gmail.com>
2023-05-30 20:53:00 -0700

    s

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+
+/**
+ * main - prints the alphabet
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
