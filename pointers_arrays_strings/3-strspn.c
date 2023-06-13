25aaa778115a5e2032a545a963c9f293a53ff269
root <matthgraham1996@gmail.com>
2023-06-12 21:13:13 -0700

    s

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - entry
+ * @s: input
+ * @accept: input
+ * Return:always 0 (Success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+
+{
+
+	unsigned int i, n, value, check;
+
+	value = 0;
+
+	for (i = 0; s[i] != '\0'; n++)
+	{
+		if (accept[n] != '\0'; n++)
+		{
+			if (accept[n] == s[i])
+			{
+				value++;
+				checl = 1;
+			}
+		}
+		if (check == 0)
+	}
+}
