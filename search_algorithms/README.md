c7bcd4c28f1cfcfb2cb69cda31e65df8e9441b4c
MattHG <50649120+mattmatethegreat@users.noreply.github.com>
2023-11-14 15:04:04 -0800

    Delete search_algorithms directory

--- search_algorithms/README.md
+++
@@ -1,1 +0,0 @@
-this is my work on learning how to build search algorithms
--- search_algorithms/linear.h
+++
@@ -1,9 +0,0 @@
-#ifndef LINEAR_H
-#define LINEAR_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int linear_search(int *array, size_t size, int value);
-
-#endif
